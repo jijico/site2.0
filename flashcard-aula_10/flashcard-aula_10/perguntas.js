3387983fbf0e76008f0890c4270cf407a0fbff19
jijico <141064667+jijico@users.noreply.github.com>
2024-10-31 23:40:08 -0300

    Add files via upload

---
+++ flashcard-aula_10/flashcard-aula_10/perguntas.js
@@ -0,0 +1,36 @@
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'O HTML é uma linguagem de programação utilizada na criação de páginas da web (internet). Ele define a estrutura do site e seu conteúdo.'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é o agronegócio e com o que ele está relacionado?',
+    'O agronegócio é um termo que faz relação com a agricultura e com a pecuária. é um setor forte da economia brasileira. Nós do Paraná nos destacamos na produção de soja, milho e cana-de-açúcar.'
+)
+
+criaCartao(
+    'Química',
+    'O que estuda a Cinética Química?',
+    'A Cinética Química estuda a velocidade média das reações, que podem ser classificadas em exotérmicas (as que liberam calor) e endotérmicas (absorvem calor).'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI e se pronuncia de forma diferente (RAI). É um dos cumprimentos mais utilizados nos países colonizados pelos ingleses.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é a Embriologia Comparada?',
+    'A Embriologia Comparada é um estudo da genética e hereditariedade em que se observa a fase embrionária de um ser, animal. Tem a finalidade de mostrar o que os animaois possuem em comum.'
+
+)
+
+criaCartao(
+    'Lingua Portuguesa',
+    'Qual autor e que obra do mesmo deu início ao movimento conhecido como "REALISMO"?',
+    'É o Machado de Assis. que deu início ao REALISMO no Brasil com "Memórias Póstumas de Brás-Cubas", no final do século XIX (19). '
+)
